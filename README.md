a2b4619f267a01ce6c337664cd9fa1df3d2b8193
Natalie Po <npodrazik@sixapart.com>
2010-03-12 18:39:04 -0500

    added README, did housekeeping, cleaned up example

---
+++ README.md
@@ -0,0 +1,11 @@
+# Rousseau
+'Rousseau' is a set of utility php libraries to help use the TypePad Blog API.  
+
+
+# Installation
+* Place the index.php and tp-libraries code on the same level on your php-enabled webserver.
+* View the index in your browser.  You'll see a simple comment listing from my Hobbitted "Plotzed" blog post. 
+
+
+
+
--- index.php
+++ index.php
@@ -1,30 +1,28 @@
 <html>
     <head>
         <title>braided</title>
-        
-        <?php
-            include_once('tp-libraries/tp-comment.php');
-        ?>
-        
+        <?php include_once('tp-libraries/tp-comment.php'); ?>
     </head>
         
-        
     <body>
-        <h2>TypePad Demo!</h2>
+        <h2>Rousseau Demo</h2>
         
+        <h4>Here are the comments for the blog post <a href="http://nataliepo.typepad.com/hobbitted/2010/01/plotzed.html">Plotzed</a>
+        from my <a href="http://nataliepo.typepad.com/hobbitted">Hobbited</a> blog.</h4>
         
         <?php
-        
-            $post_xid = '6a0120a7ee9b66970b0120a921523b970b';
+            $post_xid = '6a00e5539faa3b88330120a7b004e2970b';
                 
-            if ( ($_SERVER['REQUEST_METHOD'] == 'GET') &&
+          /*  if ( ($_SERVER['REQUEST_METHOD'] == 'GET') &&
                 ( $_GET['xid'] != '')) {
                     $post_xid = $_GET['xid'];
             }
+        */
             
             
             $comment_listing = new CommentListing($post_xid);
             $comments = $comment_listing->comments();
+
             
 //            for ($i = 0; $i < sizeof($comments); $i++) {
             foreach ($comments as $comment) {
